19a1963a7c400a581f04b0d839ab408efae9458f
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-01-26 21:06:46 +0000

    feat(iso8601): add iso8601 function

--- index.ts
+++ index.ts
@@ -123,3 +123,69 @@ export const floorSeconds2 = formatTime.floorSeconds2;
 /** Rounds the seconds portion of the specified {@link Timestamp} down to the
  * next lower millisecond, and formats the result as a 2.3-digit string. */
 export const secondsMs = formatTime.secondsMs;
+
+/** Options for formatting a {@link DateTime} in ISO 8601 format.
+ *
+ * @see iso8601 */
+export interface Iso8601Options {
+    /** Whether to use the "basic" or "extended" ISO 8601 format. In the
+     * "basic" format, colons and hyphens are omitted.
+     *
+     * @default "extended" */
+    readonly format?: "basic" | "extended" | undefined;
+
+    /** Whether to round the time down before formatting.
+     *
+     * If set to `"none"`, no rounding is performed.
+     *
+     * If set to `"seconds"`, the time is rounded down to the next lower
+     * second.
+     *
+     * If set to `"ms"`, the time is rounded down to the next lower
+     * millisecond.
+     *
+     * @default "none" */
+    readonly round?: "none" | "seconds" | "ms" | undefined;
+
+    /** Whether to delimit the time portion of the {@link DateTime} with a
+     * capital `"T"` or a space `" "`.
+     *
+     * @default "T" */
+    readonly timeDelimiter?: "T" | " " | undefined;
+}
+
+/** Returns a {@link DateTimeFormatter} that formats the specified
+ * {@link DateTime} as ISO 8601, with the specified options.
+ *
+ * By default, the {@link DateTime} is formatted in the "extended" ISO 8601
+ * format, with the time delimited by `"T"`, and without rounding, for example
+ * `"2024-01-26T11:57:23.723615"`.
+ *
+ * If the `format` option is set to `"basic"`, then the hyphens and colons are
+ * omitted, for example `"20240126T115723.723615"`.
+ *
+ * If the `round` option is set to `"seconds"`, then the time is rounded down
+ * to the next lower second, for example `"2024-01-26T11:57:23"`.
+ *
+ * If the `round` option is set to `"ms"`, then the time is rounded down to
+ * the next lower millisecond, for example `"2024-01-26T11:57:23.723"`.
+ *
+ * If the `timeDelimiter` option is set to `" "`, then the time is delimited by
+ * a space instead of by `"T"`, for example `"2024-01-26 11:57:23.363215"`.*/
+export function iso8601(options: Iso8601Options = {}): DateTimeFormatter {
+    const dateSeparator = {
+        basic: () => "",
+        extended: () => "-"
+    }[options.format ?? "extended"];
+    const timeDelimiter = (): string => options.timeDelimiter ?? "T";
+    const timeSeparator = {
+        basic: () => "",
+        extended: () => ":"
+    }[options.format ?? "extended"];
+    const seconds = {
+        none: seconds2,
+        seconds: floorSeconds2,
+        ms: secondsMs
+    }[options.round ?? "none"];
+    return dateTimeTemplate`${year4}${dateSeparator}${month2}${dateSeparator}${day2}${timeDelimiter}${hours2}${timeSeparator}${minutes2}${timeSeparator}${seconds}`;
+}
